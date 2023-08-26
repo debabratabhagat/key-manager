70d01edc25b838ac31dcc8e592e271c21f50069c
debabratabhagat <debabrata.db16@gmail.com>
2023-08-26 17:03:01 +0530

    Add component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+// import { BrowserRouter as Router,Link,Routes,Route } from 'react-router-dom';
+import React,{useState}from 'react';
 
 function App() {
+
+  const [name,setName]= useState("debabrata")
+
+  const newname = () =>{
+    setName("Rahul")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name}</h1>
+      <button onClick={()=> newname()}>change name</button>
     </div>
   );
 }

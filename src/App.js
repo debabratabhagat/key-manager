1c017f3833de30011c0e056d058203473a3bcebb
abhijeetsourav <122fp0881@nitrkl.ac.in>
2023-09-09 22:23:17 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import "./App.css";
 import Login from "./components/login/login";
 import Signup from "./components/signup/signup";
 import Signout from "./components/signout";
-import { AuthContext, AuthProvider } from "./components/auth";
+import { AuthProvider } from "./components/auth";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { useContext, useEffect, useState } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase";
-import Navbar from "./components/navbar";
+import React from "react";
 import Name from "./components/main/db";
+import LoadingSign from "./components/loader/loader";
 
 function App() {
-  const user = useContext(AuthContext);
+  // const user = useContext(AuthContext);   #########
 
   // useEffect(()=>{
 
@@ -22,12 +20,11 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Name />} />
-          <Route path="/login" element={<Login />} />
+          {/* <Route path="/" element={<LoadingSign />} /> */}
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Name />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signout" element={<Signout />} />
-          {/* <Route path="/" element={<Navbar />}>
-          {/* </Route> */}
         </Routes>
       </BrowserRouter>
     </AuthProvider>

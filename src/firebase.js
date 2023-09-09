8861dccd3054cf1a6d133eb990c1263d04cc34db
abhijeetsourav <132803890+abhijeetsourav@users.noreply.github.com>
2023-09-09 22:40:56 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,27 +3,27 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBVhpqjCB1HmBkAoLQWyhQ0zy1J3q3S0E0",
-//   authDomain: "my-first-firebase-projec-f2244.firebaseapp.com",
-//   databaseURL:
-//     "https://my-first-firebase-projec-f2244-default-rtdb.firebaseio.com",
-//   projectId: "my-first-firebase-projec-f2244",
-//   storageBucket: "my-first-firebase-projec-f2244.appspot.com",
-//   messagingSenderId: "915781204773",
-//   appId: "1:915781204773:web:d829de456a6f00b3fab8cc",
-//   measurementId: "G-K3H2ZE1J84",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBPihN5uihyRpg_5oA0r7cYNJEIslCxCOs",
-  authDomain: "cyborg-keys-app.firebaseapp.com",
-  projectId: "cyborg-keys-app",
-  storageBucket: "cyborg-keys-app.appspot.com",
-  messagingSenderId: "141212332429",
-  appId: "1:141212332429:web:bea396a7faed1536ba8dc4",
-  measurementId: "G-YDQL9KJGNL",
+  apiKey: "AIzaSyBVhpqjCB1HmBkAoLQWyhQ0zy1J3q3S0E0",
+  authDomain: "my-first-firebase-projec-f2244.firebaseapp.com",
+  databaseURL:
+    "https://my-first-firebase-projec-f2244-default-rtdb.firebaseio.com",
+  projectId: "my-first-firebase-projec-f2244",
+  storageBucket: "my-first-firebase-projec-f2244.appspot.com",
+  messagingSenderId: "915781204773",
+  appId: "1:915781204773:web:d829de456a6f00b3fab8cc",
+  measurementId: "G-K3H2ZE1J84",
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBPihN5uihyRpg_5oA0r7cYNJEIslCxCOs",
+//   authDomain: "cyborg-keys-app.firebaseapp.com",
+//   projectId: "cyborg-keys-app",
+//   storageBucket: "cyborg-keys-app.appspot.com",
+//   messagingSenderId: "141212332429",
+//   appId: "1:141212332429:web:bea396a7faed1536ba8dc4",
+//   measurementId: "G-YDQL9KJGNL",
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
